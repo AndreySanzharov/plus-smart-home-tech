f97419a2931054ab58e47a76eb0fb92e08ced889
AndreySanzharov <Andrsan2004@yandex.ru>
2025-04-09 15:37:29 +0300

    4-analyzer fix

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/service/ConditionServiceImpl.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/service/ConditionServiceImpl.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.service;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.yandex.practicum.kafka.telemetry.event.ScenarioConditionAvro;
